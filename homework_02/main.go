7498e08658b8278c6695fff1a471b9a5b41278ba
daryakuznetsova <daryamscw@gmail.com>
2024-02-09 00:59:53 +0300

    Change homework 2 solution

--- homework_02/main.go
+++ homework_02/main.go
@@ -26,17 +26,7 @@ func main() {
 		}
 	}
 
-	keySlice := make([]rune, 0)
-	valueSlice := make([]int, 0)
-	percentSlice := make([]float64, 0)
-
 	for key, v := range letterCounter {
-		percentSlice = append(percentSlice, float64(v)/float64(length))
-		keySlice = append(keySlice, key)
-		valueSlice = append(valueSlice, v)
-	}
-
-	for i := 0; i < len(letterCounter); i++ {
-		fmt.Printf("%c: %v %0.2f \n", keySlice[i], valueSlice[i], percentSlice[i])
+		fmt.Printf("%c: %v %0.2f \n", key, v, float64(v)/float64(length))
 	}
 }
